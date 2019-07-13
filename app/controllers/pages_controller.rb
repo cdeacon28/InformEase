42fe6064a9a59276faa0377681b0b0fe2c54b940
NicolasDontigny <nicolas.dontigny1@gmail.com>
2019-07-13 13:24:43 -0400

    added dependent destroy in the models

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'gruff'
+
 class PagesController < ApplicationController
   def index
     @issues = Issue.all
@@ -18,11 +20,35 @@ class PagesController < ApplicationController
   def submit_quizz
     current_user.has_taken_quizz = true
 
+    Score.where(user: current_user).destroy_all
+
+    Party.all.each do |party|
+      score = 0
+      party.party_opinions.each do |party_opinion|
+        party_stance = party_opinion.stance
+        user_stance = current_user.user_opinions.find_by(issue: party_opinion.issue).stance
+        score += 1 if party_stance == user_stance
+      end
+
+      Score.create(
+        score: score,
+        user: current_user,
+        party: party
+      )
+    end
+
     redirect_to results_path
   end
 
   def results
     @user_opinions = UserOpinion.where(user: current_user)
+    @scores = Score.where(user: current_user)
+
+    # Find out how many issues the Liberal have answered the same as the user
+
+    g = Gruff::Pie.new
+    g.title = 'Your Quiz Results'
+    g
 
     @parties = Party.all
   end
