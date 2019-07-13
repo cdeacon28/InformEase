b87c3fb5a706e6e07a34fdea750f3acc586f80b5
NicolasDontigny <nicolas.dontigny1@gmail.com>
2019-07-12 22:38:15 -0400

    added has_many to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_opinions, dependent: :destroy
+  has_many :issues, through: :user_opinions
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
