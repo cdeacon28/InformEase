bff6170894110c35d39ec3f5d72027e77a26bd67
Alina <alina.gotcherian@gmail.com>
2019-07-13 00:01:22 -0400

    added party index - wip

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2019_07_13_021745) do
 
   create_table "issues", force: :cascade do |t|
     t.string "name"
-    t.text "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
