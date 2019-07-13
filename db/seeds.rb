554e0ff448da9b812372ce9eb6ebc8d1be8d3ba0
Claire Deacon <clairelouisebd@gmail.com>
2019-07-13 00:25:34 -0400

    seed destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Party.destroy_all
+Issue.destroy_all
+PartyOpinion.destroy_all
+
 liberal = Party.create!(
   name: 'Liberal Party of Canada'
 )
