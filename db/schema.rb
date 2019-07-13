0b3701831c0f5942870a4085995cee57634bc04e
Charline <charlinem@live.fr>
2019-07-13 16:58:53 +0200

    upadte

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_07_13_021745) do
 
   create_table "issues", force: :cascade do |t|
     t.string "name"
+    t.text "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
