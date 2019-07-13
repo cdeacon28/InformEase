b4c3bb662499df8bece047a6daa8e8147a3ba185
NicolasDontigny <nicolas.dontigny1@gmail.com>
2019-07-13 11:51:50 -0400

    party filter works

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -6,4 +6,13 @@ class PartiesController < ApplicationController
   def show
     @parties = Party.find(params[:id])
   end
+
+  def filter
+    @parties = params[:party] == 'all' ? Party.all : Party.where(id: params[:party])
+
+    respond_to do |format|
+      format.html { redirect_to parties_path }
+      format.js { render 'filter.js.erb' }
+    end
+  end
 end
