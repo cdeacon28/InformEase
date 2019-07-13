d003b15e8eed62de890afd2ab36ec1dad1cbde97
Claire Deacon <clairelouisebd@gmail.com>
2019-07-12 23:32:30 -0400

    minor fixes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def index
+
+  end
+
+  def quizz
+    # @user_opinions = current_user.user_opinions.where(is_interested_in: true)
+    # @issues = current_user.issues.select { |issue|
+  end
+
+end
